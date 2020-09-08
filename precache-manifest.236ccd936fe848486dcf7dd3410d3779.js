b0442f6623afff280e5845e2eda4992507ea3989
martinsmessias <messiasads2017@gmail.com>
2020-09-08 12:46:17 -0500

    Updates

---
+++ precache-manifest.236ccd936fe848486dcf7dd3410d3779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30d9019d2361f37b6215a0746fc6706",
+    "url": "/slim-fm-react/index.html"
+  },
+  {
+    "revision": "8f1a7c2d68b9a52171b6",
+    "url": "/slim-fm-react/static/css/main.59396185.chunk.css"
+  },
+  {
+    "revision": "aab17091ba7c4474baeb",
+    "url": "/slim-fm-react/static/js/2.6693b8c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/slim-fm-react/static/js/2.6693b8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f1a7c2d68b9a52171b6",
+    "url": "/slim-fm-react/static/js/main.5bc69870.chunk.js"
+  },
+  {
+    "revision": "13d926d4164c71221dff",
+    "url": "/slim-fm-react/static/js/runtime-main.2ec75d73.js"
+  }
+]);
