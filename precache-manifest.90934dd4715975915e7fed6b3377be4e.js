3d7b0ac818d4b5224fd47077ec6c3a40b466e848
MartinsMessias <messiasads2017@gmail.com>
2020-09-08 19:38:06 -0500

    Updates

--- precache-manifest.90934dd4715975915e7fed6b3377be4e.js
+++ precache-manifest.90934dd4715975915e7fed6b3377be4e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fd0dc25fafe5b7acf8c7763a22aa4e29",
+    "revision": "be37556ae24054c8eb7eeb98559bd554",
     "url": "/slim-fm-react/index.html"
   },
   {
-    "revision": "6fee6875b8f3431e17ed",
+    "revision": "103c3d8c25f5c27612fa",
     "url": "/slim-fm-react/static/css/main.863f82b7.chunk.css"
   },
   {
-    "revision": "56da152da9009a386470",
-    "url": "/slim-fm-react/static/js/2.0d18eec3.chunk.js"
+    "revision": "52e69d9db06483e5590b",
+    "url": "/slim-fm-react/static/js/2.d7b61842.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/slim-fm-react/static/js/2.0d18eec3.chunk.js.LICENSE.txt"
+    "url": "/slim-fm-react/static/js/2.d7b61842.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6fee6875b8f3431e17ed",
-    "url": "/slim-fm-react/static/js/main.d0a2ad41.chunk.js"
+    "revision": "103c3d8c25f5c27612fa",
+    "url": "/slim-fm-react/static/js/main.0c92b230.chunk.js"
   },
   {
     "revision": "13d926d4164c71221dff",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/slim-fm-react/precache-manifest.b543af821d137d488dcc55fbfc9967a4.js"
+  "/slim-fm-react/precache-manifest.90934dd4715975915e7fed6b3377be4e.js"
 );
 
 self.addEventListener('message', (event) => {
