796dc61f5bbd041f62f575aae35469a769e2170b
MartinsMessias <messiasads2017@gmail.com>
2020-09-08 19:37:36 -0500

    Resolvendo erros eslint

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, HashRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import React from "react";
 import SearchPage from "./pages/SearchPage";
 import SearchHistoryPage from "./pages/SearchHistoryPage";
@@ -7,12 +7,12 @@ import AlbumsPage from "./pages/AlbunsPage";
 
 function Routes() {
   return (
-    <HashRouter>
+    <Switch>
       <Route path="/" exact component={SearchPage} />
       <Route path="/search-artist/:artist" component={ArtistsPage} />
       <Route path="/search-album/:album" component={AlbumsPage} />
       <Route path="/search-history" component={SearchHistoryPage} />
-    </HashRouter>
+    </Switch>
   );
 }
 
